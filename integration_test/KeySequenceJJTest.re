santilococo/oni2

commit f5f848a027b563faa0bbd107de1858207fd3fd44

refactor(vim): Synchronize vim modes (#2593)
This change is the next step from #2584 - when the mode provided by the `Editor.t` is different from the 'real' Vim state - synchronize it. 

Brings in https://github.com/onivim/libvim/pull/231 so that we can change the visual / select easily as part of that synchronization.

src/Store/VimStoreConnector.re
@@ -168,8 +168,8 @@ let start =
             Feature_Layout.activeEditor(getState().layout) |> Editor.getId;
 
           switch (newMode) {
-          | Visual({range, _})
-          | Select({range, _}) =>
+          | Visual(range)
+          | Select(range) =>
             dispatch(
               Editor({
                 scope: Oni_Model.EditorScope.Editor(editorId),

src/Model/ModeManager.re
@@ -3,7 +3,7 @@ open Oni_Core;
 module Internal = {
   let getTerminalNormalMode =
     fun
-    | Vim.Mode.Visual({range, _}) =>
+    | Vim.Mode.Visual(range) =>
       Mode.TerminalVisual({range: Oni_Core.VisualRange.ofVim(range)})
     | Vim.Mode.Operator({pending, _}) => Mode.Operator({pending: pending})
     | Vim.Mode.CommandLine => Mode.CommandLine
@@ -29,12 +29,12 @@ let current: State.t => Oni_Core.Mode.t =
                },
              )
            | Vim.Mode.Normal({cursor}) => Mode.Normal({cursor: cursor})
-           | Vim.Mode.Visual({range}) =>
+           | Vim.Mode.Visual(range) =>
              Mode.Visual({
                cursor: Vim.VisualRange.cursor(range),
                range: Oni_Core.VisualRange.ofVim(range),
              })
-           | Vim.Mode.Select({range}) =>
+           | Vim.Mode.Select(range) =>
              Mode.Select({
                cursor: Vim.VisualRange.cursor(range),
                range: Oni_Core.VisualRange.ofVim(range),

src/reason-libvim/Mode.re
@@ -5,12 +5,12 @@ type t =
   | Insert({cursors: list(BytePosition.t)})
   | CommandLine
   | Replace({cursor: BytePosition.t})
-  | Visual({range: VisualRange.t})
+  | Visual(VisualRange.t)
   | Operator({
       cursor: BytePosition.t,
       pending: Operator.pending,
     })
-  | Select({range: VisualRange.t});
+  | Select(VisualRange.t);
 
 let show = (mode: t) => {
   switch (mode) {
@@ -29,9 +29,9 @@ let cursors =
   | Normal({cursor}) => [cursor]
   | Insert({cursors}) => cursors
   | Replace({cursor}) => [cursor]
-  | Visual({range}) => [range |> VisualRange.cursor]
+  | Visual(range) => [range |> VisualRange.cursor]
   | Operator({cursor, _}) => [cursor]
-  | Select({range}) => [range |> VisualRange.cursor]
+  | Select(range) => [range |> VisualRange.cursor]
   | CommandLine => [];
 
 let current = () => {
@@ -40,7 +40,7 @@ let current = () => {
   let cursor = Cursor.get();
   switch (nativeMode) {
   | Native.Normal => Normal({cursor: cursor})
-  | Native.Visual => Visual({range: VisualRange.current()})
+  | Native.Visual => Visual(VisualRange.current())
   | Native.CommandLine => CommandLine
   | Native.Replace => Replace({cursor: cursor})
   | Native.Operator =>
@@ -49,7 +49,7 @@ let current = () => {
       pending: Operator.get() |> Option.value(~default=Operator.default),
     })
   | Native.Insert => Insert({cursors: [cursor]})
-  | Native.Select => Select({range: VisualRange.current()})
+  | Native.Select => Select(VisualRange.current())
   };
 };
 
@@ -82,3 +82,65 @@ let isOperatorPending =
   fun
   | Operator(_) => true
   | _ => false;
+
+module Internal = {
+  let ensureNormal = () =>
+    if (!isNormal(current())) {
+      Native.vimKey("<ESC>");
+      Native.vimKey("<ESC>");
+    };
+
+  let ensureInsert = () =>
+    if (!isInsert(current())) {
+      ensureNormal();
+      Native.vimKey("i");
+    };
+
+  let ensureVisual = visualType =>
+    if (!isVisual(current())) {
+      ensureNormal();
+      Types.(
+        switch (visualType) {
+        | Block => Native.vimKey("<C-v>")
+        | Line => Native.vimKey("V")
+        | Character
+        | None => Native.vimKey("v")
+        }
+      );
+    };
+
+  let ensureSelect = visualType =>
+    if (!isSelect(current())) {
+      ensureVisual(visualType);
+      Native.vimKey("<c-g>");
+    };
+};
+
+let trySet = newMode => {
+  switch (newMode) {
+  | Normal({cursor}) =>
+    Internal.ensureNormal();
+    Cursor.set(cursor);
+  | Visual(range) =>
+    Internal.ensureVisual(range.visualType);
+    Visual.set(
+      ~visualType=range.visualType,
+      ~start=range.anchor,
+      ~cursor=range.cursor,
+    );
+  | Select(range) =>
+    Internal.ensureSelect(range.visualType);
+    Visual.set(
+      ~visualType=range.visualType,
+      ~start=range.anchor,
+      ~cursor=range.cursor,
+    );
+  | Insert(_) => Internal.ensureInsert()
+  // These modes cannot be explicitly transitioned to currently
+  | Operator(_)
+  | Replace(_)
+  | CommandLine => ()
+  };
+
+  current();
+};

src/reason-libvim/Native.re
@@ -144,8 +144,11 @@ external vimUndoSync: int => unit = "libvim_vimUndoSync";
 
 external vimVisualGetRange: unit => (int, int, int, int) =
   "libvim_vimVisualGetRange";
+external vimVisualSetStart: (int, int) => unit = "libvim_vimVisualSetStart";
 
 external vimVisualGetType: unit => Types.visualType =
+  "libvim_vimVisualGetType";
+external vimVisualSetType: Types.visualType => unit =
   "libvim_vimVisualGetType";
 
 external vimWindowGetWidth: unit => int = "libvim_vimWindowGetWidth";

src/reason-libvim/Vim.re
@@ -161,7 +161,7 @@ let runWith = (~context: Context.t, f) => {
   context.lineComment |> Option.iter(Options.setLineComment);
 
   let oldBuf = Buffer.getCurrent();
-  let prevMode = Mode.current();
+  let prevMode = Mode.trySet(context.mode);
   let prevModified = Buffer.isModified(oldBuf);
   let prevLineEndings = Buffer.getLineEndings(oldBuf);
 

src/reason-libvim/Vim.rei
@@ -102,12 +102,12 @@ module Mode: {
     | Insert({cursors: list(BytePosition.t)})
     | CommandLine
     | Replace({cursor: BytePosition.t})
-    | Visual({range: VisualRange.t})
+    | Visual(VisualRange.t)
     | Operator({
         cursor: BytePosition.t,
         pending: Operator.pending,
       })
-    | Select({range: VisualRange.t});
+    | Select(VisualRange.t);
 
   let current: unit => t;
 

src/reason-libvim/Visual.re
@@ -26,3 +26,18 @@ let getRange = () => {
 };
 
 let getType = Native.vimVisualGetType;
+
+let set =
+    (
+      ~visualType: Types.visualType,
+      ~start: BytePosition.t,
+      ~cursor: BytePosition.t,
+    ) => {
+  ();
+  Native.vimVisualSetType(visualType);
+  Native.vimVisualSetStart(
+    start.line |> LineNumber.toOneBased,
+    start.byte |> ByteIndex.toInt,
+  );
+  Cursor.set(cursor);
+};

src/reason-libvim/bindings.c
@@ -1129,6 +1129,17 @@ CAMLprim value libvim_vimOptionGetTabSize(value unit) {
   return Val_int(tabSize);
 }
 
+CAMLprim value libvim_vimVisualSetStart(value vLine, value vByte) {
+    CAMLparam2(vLine, vByte);
+
+    pos_T start;
+    start.lnum = Int_val(vLine);
+    start.col = Int_val(vByte);
+    vimVisualSetStart(start);
+
+    CAMLreturn(Val_unit);
+}
+
 CAMLprim value libvim_vimVisualGetRange(value unit) {
   CAMLparam0();
   CAMLlocal1(ret);
@@ -1229,6 +1240,31 @@ CAMLprim value libvim_vimUndoSaveRegion(value startLine, value endLine) {
   int success = vimUndoSaveRegion(start, end);
 
   CAMLreturn(Val_bool(success != FAIL));
+}
+
+CAMLprim value libvim_vimVisualSetType(value vType) {
+    CAMLparam1(vType);    
+
+    char visualType = 0;
+    switch(Int_val(vType)) {
+    // character
+    case 0:
+      visualType ='v';
+      break;
+        // line
+    case 1:
+      visualType = 'V';
+      break;
+    case 2:
+       visualType = Ctrl_V;
+       break;
+    }
+
+    if (visualType != 0) {
+        vimVisualSetType(visualType);
+    }
+
+    CAMLreturn(Val_unit);
 }
 
 CAMLprim value libvim_vimVisualGetType(value unit) {

commit 57ed6f5233b3b25517689f710e196c40e05062b9

fix(input/#2601): vim.esc command not working as expected (#2603)
__Issue:__ Keybindings that use `vim.esc`, like:
```
   { "key": "jk", "command": "vim.esc", when: "insertMode" },
```

were not working as expected - it'd look like the editor would be back in normal mode, but subsequent characters would be inserted again.

__Defect:__ This was a regression from https://github.com/onivim/oni2/pull/2593 - in the `vim.esc` case, we weren't telling the editor that the mode has switched to normal. The editor would still be in insert mode, which would be synchronized on a subsequent press.

__Fix:__ Keep the editor up-to-date when the mode changes. Update an integration test to cover this.

Thanks @CrossR for the initial investigation! Fixes #2601

src/Store/VimStoreConnector.re
@@ -709,7 +709,8 @@ let start =
 
   let escapeEffect =
     Isolinear.Effect.create(~name="vim.esc", () => {
-      let _: (Vim.Context.t, list(Vim.Effect.t)) = Vim.key("<esc>");
+      let ({mode, _}: Vim.Context.t, effects) = Vim.key("<esc>");
+      updateActiveEditorMode(mode, effects);
       ();
     });
 

integration_test/KeySequenceJJTest.re
@@ -11,7 +11,7 @@ let keybindings =
 runTest(
   ~keybindings,
   ~name="KeySequenceJJTest",
-  (dispatch, wait, _) => {
+  (dispatch, wait, runEffects) => {
     wait(~name="Initial mode is normal", (state: State.t) =>
       Feature_Vim.mode(state.vim) |> Vim.Mode.isNormal
     );
@@ -27,6 +27,7 @@ runTest(
       dispatch(Model.Actions.KeyDown(keyPress, time));
       //dispatch(Model.Actions.TextInput(key));
       dispatch(Model.Actions.KeyUp(keyPress, time));
+      runEffects();
     };
 
     input("i");
@@ -34,20 +35,46 @@ runTest(
       Feature_Vim.mode(state.vim) |> Vim.Mode.isInsert
     );
 
+    input("a");
     input("j");
     input("j");
 
     wait(~name="Mode is back to normal", (state: State.t) =>
       Feature_Vim.mode(state.vim) |> Vim.Mode.isNormal
     );
-    // TODO: Figure out why this check is failing...
+
     wait(~name="Validate buffer is empty", (state: State.t) => {
       let actual =
         Model.Selectors.getActiveBuffer(state)
         |> Option.map(Core.Buffer.getLines)
         |> Option.map(Array.to_list);
 
-      actual == Some([]);
+      actual == Some(["a"]);
+    });
+
+    wait(~name="#2601: Validate editor mode is normal, too", (state: State.t) => {
+      let editorMode =
+        state.layout
+        |> Feature_Layout.activeEditor
+        |> Feature_Editor.Editor.mode;
+
+      Vim.Mode.isNormal(editorMode);
+    });
+
+    // #2601 - Make sure we're _actually_ in normal mode!
+    // Type another 'j' to see...
+    input("j");
+
+    wait(
+      ~name=
+        "#2601: Buffer should _still_ be empty, since we used j in normal mode",
+      (state: State.t) => {
+      let actual =
+        Model.Selectors.getActiveBuffer(state)
+        |> Option.map(Core.Buffer.getLines)
+        |> Option.map(Array.to_list);
+
+      actual == Some(["a"]);
     });
   },
 );
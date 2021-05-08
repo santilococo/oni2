santilococo/oni2

commit 9ab419e36ed034d555b7234aceb3812ec339f2e9

fix(codelens/#3468): Make codelens clickable (#3482)
- Make codelens clickable and trigger the contributed command (related #3468 )

src/Feature/Editor/Editor.re
@@ -28,6 +28,7 @@ type inlineElement = {
   view:
     (~theme: Oni_Core.ColorTheme.Colors.t, ~uiFont: UiFont.t, unit) =>
     Revery.UI.element,
+  command: option(Exthost.Command.t),
 };
 
 module WrapMode = {
@@ -985,13 +986,14 @@ let withSteadyCursor = (f, editor) => {
   };
 };
 
-let makeInlineElement = (~key, ~uniqueId, ~lineNumber, ~view) => {
+let makeInlineElement = (~command=None, ~key, ~uniqueId, ~lineNumber, ~view) => {
   hidden: false,
   reconcilerKey: Brisk_reconciler.Key.create(),
   key,
   uniqueId,
   lineNumber,
   view,
+  command,
 };
 
 let linesWithInlineElements = ({inlineElements, _}) => {
@@ -1039,6 +1041,7 @@ let replaceInlineElements = (~key, ~startLine, ~stopLine, ~elements, editor) => 
            height: Component_Animation.make(Animation.expand(0., 0.)),
            view: inlineElement.view,
            opacity: Component_Animation.make(Animation.fadeIn),
+           command: inlineElement.command,
          }
        );
   editor
@@ -1073,6 +1076,7 @@ let setCodeLens = (~startLine, ~stopLine, ~handle, ~lenses, editor) => {
              ~codeLens=lens,
            );
          makeInlineElement(
+           ~command=lens.command,
            ~key="codelens:" ++ string_of_int(handle),
            ~uniqueId,
            ~lineNumber,

src/Feature/Editor/EditorSurface.re
@@ -324,6 +324,35 @@ let%component make =
   let horizontalScrollbarThickness =
     Editor.horizontalScrollbarThickness(editor);
 
+  let topVisibleLine = Editor.getTopVisibleBufferLine(editor);
+  let bottomVisibleLine = Editor.getBottomVisibleBufferLine(editor);
+
+  let rec getInlineElements = (acc: list(Revery.UI.element), lines) =>
+    switch (lines) {
+    | [] => acc
+    | [line, ...tail] =>
+      let isVisible = line >= topVisibleLine && line <= bottomVisibleLine;
+      getInlineElements(
+        [
+          <InlineElementView.Container
+            config
+            uiFont
+            theme
+            editor
+            line
+            dispatch
+            isVisible
+          />,
+          ...acc,
+        ],
+        tail,
+      );
+    };
+
+  let linesWithElements = Editor.linesWithInlineElements(editor);
+
+  let lensElements = getInlineElements([], linesWithElements);
+
   <View
     style={Styles.container(~colors)}
     onDimensionsChanged
@@ -351,9 +380,8 @@ let%component make =
       bufferPixelWidth={int_of_float(layout.bufferWidthInPixels)}
       windowIsFocused
       config
-      uiFont
-      theme
     />
+    {lensElements |> React.listToElement}
     {Editor.isMinimapEnabled(editor)
        ? <minimap
            editor

src/Feature/Editor/InlineElementView.re
@@ -1,17 +1,20 @@
 open EditorCoreTypes;
 open Revery.UI;
+module Clickable = Components.Clickable;
 
 module LineNumber = EditorCoreTypes.LineNumber;
 
 module Styles = {
   open Style;
-  let container = (~opacity as opac, ~pixelY) => {
+  let container = (~opacity as opac, ~totalHeight, ~pixelY) => {
     [
       position(`Absolute),
       top(int_of_float(pixelY)),
       left(0),
       right(0),
+      height(totalHeight),
       opacity(opac),
+      pointerEvents(`Allow),
     ];
   };
 
@@ -23,12 +26,13 @@ module Styles = {
       bottom(0),
       height(h),
       opacity(0.8),
+      pointerEvents(`Ignore),
     ];
   };
 
   let inner = (~opacity as opac, ~yOffset) => [
     position(`Absolute),
-    bottom(int_of_float(yOffset)),
+    top(int_of_float(yOffset)),
     left(0),
     right(0),
     flexDirection(`Row),
@@ -69,7 +73,6 @@ module Item = {
           None;
         },
       );
-
     // COMPONENT
     <View
       style={Styles.inner(~yOffset, ~opacity)}
@@ -89,10 +92,34 @@ module Container = {
       |> List.fold_left(
            (acc, inlineElement: InlineElements.element) => {
              let (currentOpacity, height, accElements) = acc;
+             let h = height;
              let uniqueId = inlineElement.uniqueId;
              let elem = inlineElement.view(~theme, ~uiFont);
              let inlineKey = inlineElement.key;
              let opacity = inlineElement.opacity |> Component_Animation.get;
+             let currentHeight =
+               Component_Animation.get(inlineElement.height);
+             let clickable =
+               <Clickable
+                 style=Style.[
+                   position(`Absolute),
+                   top(int_of_float(h)),
+                   left(0),
+                   right(0),
+                   height(int_of_float(currentHeight)),
+                   pointerEvents(`Allow),
+                 ]
+                 onClick={_ =>
+                   dispatch(
+                     Msg.InlineElementClicked({
+                       key: inlineKey,
+                       uniqueId,
+                       command: inlineElement.command,
+                     }),
+                   )
+                 }>
+                 React.empty
+               </Clickable>;
 
              let newElement =
                <Item
@@ -107,8 +134,8 @@ module Container = {
 
              (
                max(currentOpacity, opacity),
-               height +. Component_Animation.get(inlineElement.height),
-               [newElement, ...accElements],
+               height +. currentHeight,
+               [newElement, clickable, ...accElements],
              );
            },
            (0., 0., []),
@@ -141,7 +168,12 @@ module Container = {
           </View>
         : React.empty;
 
-    <View style={Styles.container(~opacity=maxOpacity, ~pixelY)}>
+    <View
+      style={Styles.container(
+        ~opacity=maxOpacity,
+        ~totalHeight=int_of_float(totalHeight),
+        ~pixelY=pixelY -. totalHeight,
+      )}>
       {elems |> React.listToElement}
       shadow
     </View>;

development_extensions/oni-dev-extension/extension.js
@@ -142,6 +142,25 @@ function activate(context) {
         }),
     )
 
+    cleanup(
+        vscode.languages.registerCodeLensProvider("oni-dev", {
+            provideCodeLenses: (document, token) => {
+                const range = new vscode.Range(1, 1, 1, 1);
+                const codeLens = vscode.CodeLens(range, {
+                    command: "developer.oni.codeLensClicked1",
+                    title: "Test"
+                })
+                return [codeLens]
+            },
+        }),
+    );
+
+    cleanup(
+        vscode.commands.registerCommand("developer.oni.codeLensClicked1", () => {
+            vscode.window.showInformationMessage("Code lens 1 clicked!");
+        }),
+    );
+
     // SLOW completion provider
     cleanup(
         vscode.languages.registerCompletionItemProvider("oni-dev", {

src/Feature/Editor/Editor.rei
@@ -38,6 +38,7 @@ type inlineElement;
 
 let makeInlineElement:
   (
+    ~command: option(Exthost.Command.t)=?,
     ~key: string,
     ~uniqueId: string,
     ~lineNumber: EditorCoreTypes.LineNumber.t,

src/Feature/Editor/Feature_Editor.re
@@ -29,7 +29,8 @@ type msg = Msg.t;
 type outmsg =
   | Nothing
   | MouseHovered(option(CharacterPosition.t))
-  | MouseMoved(option(CharacterPosition.t));
+  | MouseMoved(option(CharacterPosition.t))
+  | ExecuteCommand(Exthost.Command.t);
 
 type model = Editor.t;
 
@@ -111,11 +112,20 @@ let update = (editor, msg) => {
       Editor.setInlineElementSize(~key, ~line, ~uniqueId, ~height, editor),
       Nothing,
     )
+  | InlineElementClicked({command, _}) => (
+      editor,
+      switch (command) {
+      | Some(cmd) => ExecuteCommand(cmd)
+      | None => Nothing
+      },
+    )
   | PreviewChanged(preview) => (
       Editor.setPreview(~preview, editor),
       Nothing,
     )
-  | Internal(msg) => (Editor.update(msg, editor), Nothing)
+  | Internal(msg) =>
+    let editor' = Editor.update(msg, editor);
+    (editor', Nothing);
   | EditorMouseMoved({time, pixelX, pixelY}) =>
     let editor' = editor |> Editor.mouseMove(~time, ~pixelX, ~pixelY);
 

src/Feature/Editor/InlineElements.re
@@ -18,20 +18,6 @@ module Constants = {
   let maxElementsToAnimate = 100;
 };
 
-type msg =
-  | OpacityAnimation({
-      key: string,
-      line: LineNumber.t,
-      uniqueId: string,
-      msg: Component_Animation.msg,
-    })
-  | HeightAnimation({
-      key: string,
-      line: LineNumber.t,
-      uniqueId: string,
-      msg: Component_Animation.msg,
-    });
-
 [@deriving show]
 type element = {
   key: string,
@@ -42,6 +28,7 @@ type element = {
   view:
     (~theme: Oni_Core.ColorTheme.Colors.t, ~uiFont: UiFont.t, unit) =>
     Revery.UI.element,
+  command: option(Exthost.Command.t),
 };
 
 // A map of inline element key -> inline elements
@@ -420,22 +407,6 @@ let updateElement =
        }),
      );
 };
-
-let update = (msg, model) =>
-  switch (msg) {
-  | OpacityAnimation({key, uniqueId, line, msg}) =>
-    let updateOpacity = element => {
-      ...element,
-      opacity: Component_Animation.update(msg, element.opacity),
-    };
-    updateElement(~key, ~uniqueId, ~line, ~f=updateOpacity, model);
-  | HeightAnimation({key, uniqueId, line, msg}) =>
-    let updateHeight = element => {
-      ...element,
-      height: Component_Animation.update(msg, element.height),
-    };
-    updateElement(~key, ~uniqueId, ~line, ~f=updateHeight, model);
-  };
 
 let setSize = (~animated, ~key, ~line, ~uniqueId, ~height, model) => {
   let setHeight = (curr: element) => {

src/Feature/Editor/Msg.re
@@ -53,5 +53,10 @@ type t =
       line: EditorCoreTypes.LineNumber.t,
       height: int,
     })
+  | InlineElementClicked({
+      key: string,
+      uniqueId: string,
+      command: option(Exthost.Command.t),
+    })
   | BoundingBoxChanged({bbox: [@opaque] Revery.Math.BoundingBox2d.t})
   | Internal(Editor.msg);

commit 200dec347121876f5019f8acd01e642091ddf66e

fix(codelens): Fix indentation issue with codelens (#3517)
Fix codelens indentation introduced in #3482

src/Feature/Editor/Editor.re
@@ -200,18 +200,6 @@ let isAnimatingScroll = ({scrollX, scrollY, _}) => {
   Spring.isActive(scrollX) || Spring.isActive(scrollY);
 };
 
-let getLeadingWhitespacePixels = (lineNumber, editor) => {
-  let buffer = editor.buffer;
-  let lineCount = EditorBuffer.numberOfLines(buffer);
-  let line = lineNumber |> EditorCoreTypes.LineNumber.toZeroBased;
-  if (line < 0 || line >= lineCount) {
-    0.;
-  } else {
-    let bufferLine = buffer |> EditorBuffer.line(line);
-    BufferLine.getLeadingWhitespacePixels(bufferLine);
-  };
-};
-
 let getBufferLineCount = ({buffer, _}) =>
   EditorBuffer.numberOfLines(buffer);
 
@@ -1068,13 +1056,8 @@ let setCodeLens = (~startLine, ~stopLine, ~handle, ~lenses, editor) => {
            Feature_LanguageSupport.CodeLens.lineNumber(lens)
            |> EditorCoreTypes.LineNumber.ofZeroBased;
          let uniqueId = Feature_LanguageSupport.CodeLens.text(lens);
-         let leftMargin =
-           getLeadingWhitespacePixels(lineNumber, editor) |> int_of_float;
          let view =
-           Feature_LanguageSupport.CodeLens.View.make(
-             ~leftMargin,
-             ~codeLens=lens,
-           );
+           Feature_LanguageSupport.CodeLens.View.make(~codeLens=lens);
          makeInlineElement(
            ~command=lens.command,
            ~key="codelens:" ++ string_of_int(handle),

src/Feature/Editor/EditorSurface.re
@@ -342,6 +342,7 @@ let%component make =
             line
             dispatch
             isVisible
+            gutterWidth
           />,
           ...acc,
         ],
@@ -381,7 +382,18 @@ let%component make =
       windowIsFocused
       config
     />
-    {lensElements |> React.listToElement}
+    <View
+      style=Style.[
+        position(`Absolute),
+        top(0),
+        left(0),
+        bottom(0),
+        right(0),
+        overflow(`Hidden),
+        pointerEvents(`Ignore),
+      ]>
+      {lensElements |> React.listToElement}
+    </View>
     {Editor.isMinimapEnabled(editor)
        ? <minimap
            editor

src/Feature/Editor/InlineElementView.re
@@ -30,10 +30,10 @@ module Styles = {
     ];
   };
 
-  let inner = (~opacity as opac, ~yOffset) => [
+  let inner = (~opacity as opac, ~yOffset, ~xOffset) => [
     position(`Absolute),
     top(int_of_float(yOffset)),
-    left(0),
+    left(int_of_float(xOffset)),
     right(0),
     flexDirection(`Row),
     flexGrow(1),
@@ -48,6 +48,7 @@ module Item = {
                   ~inlineKey: string,
                   ~uniqueId: string,
                   ~opacity: float,
+                  ~xOffset: float,
                   ~yOffset: float,
                   ~lineNumber: LineNumber.t,
                   ~children,
@@ -75,7 +76,7 @@ module Item = {
       );
     // COMPONENT
     <View
-      style={Styles.inner(~yOffset, ~opacity)}
+      style={Styles.inner(~xOffset, ~yOffset, ~opacity)}
       onDimensionsChanged={({height, _}) => {heightChangedDispatch(height)}}>
       children
     </View>;
@@ -84,8 +85,21 @@ module Item = {
 
 module Container = {
   let make =
-      (~config, ~editor, ~isVisible, ~line, ~dispatch, ~theme, ~uiFont, ()) => {
+      (
+        ~gutterWidth,
+        ~config,
+        ~editor,
+        ~isVisible,
+        ~line,
+        ~dispatch,
+        ~theme,
+        ~uiFont,
+        (),
+      ) => {
     let inlineElements = Editor.getInlineElements(~line, editor);
+
+    let leadingWhitespacePixels =
+      Editor.getLeadingWhitespacePixels(line, editor);
 
     let (maxOpacity, totalHeight, elems) =
       inlineElements
@@ -127,6 +141,7 @@ module Container = {
                  uniqueId
                  dispatch
                  lineNumber=line
+                 xOffset={gutterWidth +. leadingWhitespacePixels}
                  yOffset=height
                  opacity>
                  <elem />

src/Feature/LanguageSupport/CodeLens.re
@@ -201,7 +201,7 @@ module Contributions = {
 module View = {
   module CodeLensColors = Colors;
   open Revery.UI;
-  let make = (~leftMargin, ~theme, ~uiFont: Oni_Core.UiFont.t, ~codeLens, ()) => {
+  let make = (~theme, ~uiFont: Oni_Core.UiFont.t, ~codeLens, ()) => {
     let foregroundColor = CodeLensColors.foreground.from(theme);
     let label =
       Exthost.CodeLens.(codeLens.command)
@@ -212,7 +212,6 @@ module View = {
       style=Style.[
         marginTop(4),
         marginBottom(0),
-        marginLeft(leftMargin),
         flexGrow(1),
         flexShrink(0),
       ]>

src/Feature/LanguageSupport/Feature_LanguageSupport.rei
@@ -42,7 +42,6 @@ module CodeLens: {
   module View: {
     let make:
       (
-        ~leftMargin: int,
         ~theme: Oni_Core.ColorTheme.Colors.t,
         ~uiFont: UiFont.t,
         ~codeLens: t,
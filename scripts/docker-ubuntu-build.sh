f11e2fb3d2b804dc3d02f3b64926cdcb1d4120db
Santiago Lo Coco <santilococo.01@gmail.com>
2022-03-23 23:39:46 -0300

    Add ubuntu docker files

---
+++ scripts/docker-ubuntu-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export ESY__PREFIX=/esy/store
+
+patch -p1 < "$1"
+
+node install-node-deps.js
+esy install 
+esy bootstrap
+esy build
+esy run -f --checkhealth
+
+esy '@release' install
+esy '@release' run -f --checkhealth
+esy '@release' run --help
+esy '@release' create
